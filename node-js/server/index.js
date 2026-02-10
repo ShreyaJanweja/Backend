4c0a0a188bdcdc4bdcf83ee068e2481cbddcd6b4
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-02-10 17:47:03 +0530

    handling URL's in nodeJS

--- node-js/server/index.js
+++ node-js/server/index.js
@@ -1,5 +1,5 @@
 //building HTTP server
-const http = require('http'); 
+// const http = require('http'); 
 // const myServer = http.createServer((req,res)=>{
 //     console.log(req);
 //     res.end("Hello from server Again");
@@ -12,21 +12,44 @@ const http = require('http');
 // //ab hm apne server ko access kr skte hai browser mei jaake localhost:8000 likhke
 
 
-//let's perform a task using fs module and server (jab bhi koi incoming request ayegi toh hum log bnayenge ek file jisme request ka time,data store hoga)
-const fs = require("fs");
-const server2 = http.createServer((req,res)=>{
-    if(req.url == "/favicon.io") return res.end();
-    fs.appendFile("log.txt", `Request made at: ${Date.now()}: ${req.url}\n`, (err,data)=>{
-    if(req.url === "/"){
-        res.end("Welcome to Home Page");
-    }
-    else if(req.url === "/about"){
-        res.end("I am Shreya, a backend developer");
-    }
-    else{
-        res.end("404 Not Found");
-    }
-    });
-});
+// //let's perform a task using fs module and server (jab bhi koi incoming request ayegi toh hum log bnayenge ek file jisme request ka time,data store hoga)
+// const fs = require("fs");
+// const server2 = http.createServer((req,res)=>{
+//     if(req.url == "/favicon.io") return res.end();
+//     fs.appendFile("log.txt", `Request made at: ${Date.now()}: ${req.url}\n`, (err,data)=>{
+//     if(req.url === "/"){
+//         res.end("Welcome to Home Page");
+//     }
+//     else if(req.url === "/about"){
+//         res.end("I am Shreya, a backend developer");
+//     }
+//     else{
+//         res.end("404 Not Found");
+//     }
+//     });
+// });
 
-server2.listen(8001, ()=> console.log("Server2 started at port2"));
+// server2.listen(8001, ()=> console.log("Server2 started at port2"));
+
+
+
+
+//LEC - 8 Handling URL'S (installed node modules)
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+ const server3 = http.createServer((req,res)=>{
+    if(req.url == "/favicon.ico") return res.end();
+    const myUrl = url.parse(req.url,true);
+    console.log(myUrl);
+    fs.appendFile("log.txt", `New Request: ${Date.now()}: ${req.url}\n`,(err,data)=>{
+        if(myUrl.pathname == "/") res.end("Welcome to Home Page");
+        else if(myUrl.pathname=="/about") {
+            const username = myUrl.query.myname;
+            res.end(`Hi, ${username}`);
+        }
+        else res.end("404 Not Found");
+    });
+ });
+server3.listen(8001,()=> console.log("Server3 started at port 8002"));
