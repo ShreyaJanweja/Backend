9af575af0f10de0f380b8a87d676efb4a0aa94f7
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-02-10 20:12:41 +0530

    Implemented HTTP methods

--- node-js/server/index.js
+++ node-js/server/index.js
@@ -34,6 +34,30 @@
 
 
 
+// //LEC - 8 Handling URL'S (installed node modules)
+// const http = require('http');
+// const fs = require('fs');
+// const url = require('url');
+
+//  const server3 = http.createServer((req,res)=>{
+//     if(req.url == "/favicon.ico") return res.end();
+//     const myUrl = url.parse(req.url,true);
+//     console.log(myUrl);
+//     fs.appendFile("log.txt", `New Request: ${Date.now()}: ${req.url}\n`,(err,data)=>{
+//         if(myUrl.pathname == "/") res.end("Welcome to Home Page");
+//         else if(myUrl.pathname=="/about") {
+//             const username = myUrl.query.myname;
+//             res.end(`Hi, ${username}`);
+//         }
+//         else res.end("404 Not Found");
+//     });
+//  });
+// server3.listen(8001,()=> console.log("Server3 started at port 8001"));
+
+
+
+
+//LEC-9 HTTP Mthods
 //LEC - 8 Handling URL'S (installed node modules)
 const http = require('http');
 const fs = require('fs');
@@ -43,9 +67,9 @@ const url = require('url');
     if(req.url == "/favicon.ico") return res.end();
     const myUrl = url.parse(req.url,true);
     console.log(myUrl);
-    fs.appendFile("log.txt", `New Request: ${Date.now()}: ${req.url}\n`,(err,data)=>{
+    fs.appendFile("log.txt", `New Request: ${Date.now()}: ${req.method} ${req.url}\n`,(err,data)=>{
         if(myUrl.pathname == "/") res.end("Welcome to Home Page");
-        else if(myUrl.pathname=="/about") {
+        else if(myUrl.pathname=="/about") { 
             const username = myUrl.query.myname;
             res.end(`Hi, ${username}`);
         }
