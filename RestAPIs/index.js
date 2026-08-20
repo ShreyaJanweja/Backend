69d756c080496a2071e67bc77d916e380c4c4859
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-08-20 16:50:08 +0530

    GET request in restAPIs

--- RestAPIs/index.js
+++ RestAPIs/index.js
@@ -1,4 +1,52 @@
 const express = require('express');
 const app = express();
 const PORT = 8000;
+const users = require("./MOCK_DATA.json");
+
+//routes
+//html format (web app)
+app.get("/users", (req,res)=>{
+    const html = `
+    <ul>
+        ${users.map((user)=> `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
+});
+
+
+//REST APIs
+//json format (mobile app)
+app.get("/api/users",(req,res)=>{
+    return res.json(users);
+});
+
+//get any particular user by id 
+app.get("/api/users/:id", (req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=> user.id === id);
+    return res.json(user);
+});
+
+
+// post , put, delete request cannot be made at browser, so for now will just create a router and test it using postman after creating a postman collection
+app.post("/api/users", (req,res)=>{
+    //TODO: create new user
+    return res.json({status: "pending"});
+});
+
+
+app.patch("/api/users", (req,res)=>{
+    //TODO: edit the user with id
+    return res.json({status: "pending"});
+});
+
+
+app.delete("/api/users", (req,res)=>{
+    //TODO: delete the user with id
+    return res.json({status: "pending"});
+});
+
+
+
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
