ae3a71480fd467384fb14e9828b28ec96109338d
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-08-21 00:11:38 +0530

    POST request with postman

--- RestAPIs/index.js
+++ RestAPIs/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const fs = require("fs");
 const app = express();
 const PORT = 8000;
 const users = require("./MOCK_DATA.json");
+
+//middleware - Plugin 
+app.use(express.urlencoded({extended: false})); //for parsing form data
 
 //routes
 //html format (web app)
@@ -29,10 +33,25 @@ app.get("/api/users/:id", (req,res)=>{
 });
 
 
-// post , put, delete request cannot be made at browser, so for now will just create a router and test it using postman after creating a postman collection
+// // post , put, delete request cannot be made at browser, so for now will just create a router and test it using postman after creating a postman collection
+// app.post("/api/users", (req,res)=>{
+//     //TODO: create new user 
+//     const body = req.body; //jo bhi data hum frontend se send krte hai vo 
+//     //abhi agar console.log(body) krenge to undefined aayega kyuki express ko ye nahi pata ki humne jo data bheja hai vo json format me hai, isliye hume express.urlencoded() middleware use krna padega
+//     //ye plugin kya kre ga - jo bhi form mei data ayega usse body object me convert kr dega, aur hum usse req.body se access kr skte hai
+//     //ek middleware bna rhi hu sabse top pe likh ri hu
+//     console.log('body: ',body); //jo bhi post request me data bheja hai vo yaha print hoga
+//     return res.json({status: "pending"});
+// });
+
+
+//same mock data ko apne fake data jo MOCK_DATA.json mei add krte h
 app.post("/api/users", (req,res)=>{
-    //TODO: create new user
-    return res.json({status: "pending"});
+    const body = req.body;
+    users.push({...body, id: users.length+1});
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err,data)=>{
+        return res.json({status:"pending"});
+    });
 });
 
 
